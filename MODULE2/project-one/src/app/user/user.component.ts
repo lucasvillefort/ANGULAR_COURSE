6d3477000f979dddc1d47c761cb1a470ebffe27b
lucas villefort <lucasvillefort11@gmail.com>
2024-07-16 20:34:41 -0300

    sdd

--- MODULE2/project-one/src/app/user/user.component.ts
+++ MODULE2/project-one/src/app/user/user.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { DUMMY_USERS } from '../dummy-users';
+
+// i can create a lot of funtions in order to implemnets it into my class
+const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
 
 @Component({
   selector: 'app-user',
@@ -8,5 +12,6 @@ import { Component } from '@angular/core';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
+  selectUser = DUMMY_USERS[randomIndex]
 
 }
